38c8f54c7e1db5a52cf7c790dbf7ee6afc03a058
Oskari Pölhö <osku@oskaripolho.com>
2024-05-06 16:54:40 +0300

    change string

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ const PORT = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
   const string = helloWorld();
-  res.send("Hello World in typescript");
+  res.send("Hello World in typescript - using .env");
 });
 
 app.listen(PORT, () => {
